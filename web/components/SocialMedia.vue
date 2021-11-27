227958a4dd7e1bd6e465dca25b1e7549f3382d6d
Constant V <constantvallee@gmail.com>
2021-11-27 13:06:30 -0500

    feat: add socials component

---
+++ web/components/SocialMedia.vue
@@ -0,0 +1,59 @@
+<template>
+    <ul class="social__wrapper">
+        <li v-for="social in socials" :key="social._key" class="social">
+        <a :href="social.url"
+            ><IconBase :icon-name="social.title" width="44" height="44"
+            ><component :is="'Icon' + social.title" /></IconBase
+        ></a>
+        </li>
+    </ul>
+</template>
+
+<script>
+import IconBase from '../components/IconBase'
+import IconInsta from '../components/Icons/IconInsta'
+import IconVimeo from '../components/Icons/IconVimeo'
+import IconYoutube from '../components/Icons/IconYoutube'
+import IconFacebook from '../components/Icons/IconFacebook'
+
+export default {
+  components: {
+    IconBase,
+    IconInsta,
+    IconVimeo,
+    IconFacebook,
+    IconYoutube,
+  },
+  computed: {
+    socials() {
+      return this.$store.state.socials
+    },
+  },
+}
+</script>
+
+<style>
+.social__wrapper {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 2rem 0;
+}
+
+.social {
+  padding-right: 2rem;
+
+  & a {
+    color: #6a6052;
+  }
+
+  & a:visited {
+    color: #6a6052;
+  }
+
+  & a:hover,
+  & a:focus {
+    color: #35495e;
+  }
+}
+</style>
--- web/pages/contact.vue
+++ web/pages/contact.vue
@@ -3,14 +3,7 @@
     <div class="slide__container">
       <div class="slide">
         <h2 class="title">Socials</h2>
-        <ul class="social__wrapper">
-          <li v-for="social in socials" :key="social._key" class="social">
-            <a :href="social.url"
-              ><IconBase :icon-name="social.title" width="44" height="44"
-                ><component :is="'Icon' + social.title" /></IconBase
-            ></a>
-          </li>
-        </ul>
+        <SocialMedia />
       </div>
       <div class="slide">
         <h2 class="title">Contact Me</h2>
@@ -21,29 +14,13 @@
 </template>
 
 <script>
-import groq from 'groq'
-import IconBase from '../components/IconBase'
-import IconInsta from '../components/Icons/IconInsta'
-import IconVimeo from '../components/Icons/IconVimeo'
-import IconYoutube from '../components/Icons/IconYoutube'
-import IconFacebook from '../components/Icons/IconFacebook'
 import ContactForm from '../components/ContactForm'
-import sanityClient from '~/sanityClient'
+import SocialMedia from '../components/SocialMedia.vue'
 
-const query = groq`
-  *[_type == "photograph" && slug.current == "elea-jeanne-schmitter"][0]
-`
 export default {
   components: {
     ContactForm,
-    IconBase,
-    IconInsta,
-    IconVimeo,
-    IconFacebook,
-    IconYoutube,
-  },
-  async asyncData() {
-    return await sanityClient.fetch(query)
+    SocialMedia,
   },
 }
 </script>
@@ -61,27 +38,6 @@ export default {
   display: flex;
   flex-direction: column;
   position: relative;
-}
-
-.social__wrapper {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 2rem 0;
-}
-
-.social {
-  padding-right: 2rem;
-  color: #6a6052;
-
-  & a:visited {
-    color: #6a6052;
-  }
-
-  & a:hover,
-  & a:focus {
-    color: #35495e;
-  }
 }
 
 .slide__container {
